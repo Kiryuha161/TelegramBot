a5ba5e529123644808233e5d8373cff6584be75d
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2024-03-21 18:01:13 +0300

    Сделал дерево и пытаюсь его связать с кнопками

---
+++ faq.js
@@ -0,0 +1,148 @@
+module.exports.faq = {
+    sites: {
+        china: {
+            id: "1",
+            description: "Список вопросов и ответов для Viomitra.Китай",
+            name: "Viomitra.Китай",
+            categories: [{
+                userIdentification: {
+                    id: "1.1",
+                    height: "10px",
+                    name: "Авторизация и регистрация на Viomitra.Китай",
+                    subcategories: [{
+                        registration: {
+                            id: "1.1.1.1",
+                            height: "10px",
+                            name: "Регистрация на Viomitra.Китай",
+                            answers: {
+                                howRegistrate: {
+                                    id: "1.1.1.1.1",
+                                    height: "10px",
+                                    name: "Как зарегистрироваться в Viomitra.Китай",
+                                    value: "Изучите этот раздел FAQ - https://china.viomitra.ru/registration \nВам помог наш бот?"
+                                }
+                            }
+                        },
+                        authorization: {
+                            id: "1.1.1.2",
+                            height: "10px",
+                            name: "Авторизация на Viomitra.Китай",
+                            answers: {
+                                howAuthorize: {
+                                    id: "1.1.1.2.1",
+                                    height: "10px",
+                                    name: "Как авторизоваться в Viomitra.Китай",
+                                    value: "Чтобы войти в аккаунт нажмите 'Войти', после чего введите логин и пароль. \nВам помог наш бот?"
+                                },
+                                recoveryAccess: {
+                                    id: "1.1.1.2.2",
+                                    height: "10px",
+                                    name: "Как восстановить доступ в Viomitra.Китай",
+                                    value: "Изучите этот раздел FAQ - https://china.viomitra.ru/restore \nВам помог наш бот?"
+                                }
+                            }
+                        }
+                    }]
+                }, 
+                payment: {
+                    id: "1.2",
+                    height: "10px",
+                    name: "Оплата в Viomitra.Китай",
+                }
+            }]
+        },
+        bankrot: {
+            id: "2",
+            description: "Список вопросов и ответов для Viomitra.Банкроство",
+            name: "Viomitra.Банкротство",
+            categories: [{
+                userIdentification: {
+                    id: "2.1",
+                    height: "10px",
+                    name: "Авторизация и регистрация на Viomitra.Банкротство",
+                    subcategories: [{
+                        registration: {
+                            id: "2.1.1.1",
+                            height: "10px",
+                            name: "Регистрация на Viomitra.Банкротство",
+                            answers: {
+                                howRegistrate: {
+                                    id: "2.1.1.1.1",
+                                    height: "10px",
+                                    name: "Как зарегистрироваться в Viomitra.Банкроство",
+                                    value: "Скачайте этот документ с инструкцией - https://bankrot.viomitra.ru/Document/GuideDocument?guideName=RegistrationGuideDoc \nВам помог наш бот?"
+                                }
+                            }
+                        },
+                        authorization: {
+                            id: "2.1.1.2",
+                            height: "10px",
+                            name: "Авторизация на Viomitra.Банкротство",
+                            answers: {
+                                howAuthorize: {
+                                    id: "2.1.1.2.1",
+                                    height: "10px",
+                                    name: "Как авторизоваться в Viomitra.Банкротство",
+                                    value: "Чтобы войти в аккаунт нажмите 'Войти', после чего введите логин и пароль. \nВам помог наш бот?"
+                                },
+                                recoveryAccess: {
+                                    id: "2.1.1.2.2",
+                                    height: "10px",
+                                    name: "Как восстановить доступ в Viomitra.Банкротство",
+                                    value: "Если вы забыли пароль, вы можете воспользоваться формой 'Забыли пароль?' http://bankrot.viomitra.ru/Account/ForgotPassword \nВам помог наш бот?"
+                                }
+                            }
+                        }
+                    }]
+                }
+            }]
+        },
+        realty: {
+            id: "3",
+            description: "Список вопросов и ответов для Viomitra.Коммерческие торги",
+            name: "Viomitra.Коммерческие торги",
+            categories: [{
+                userIdentification: {
+                    id: "3.1",
+                    height: "10px",
+                    name: "Авторизация и регистрация на Viomitra.Коммерческие торги",
+                    subcategories: [{
+                        registration: {
+                            id: "3.1.1.1",
+                            height: "10px",
+                            name: "Регистрация на Viomitra.Коммерческие торги",
+                            answers: {
+                                howRegistrate: {
+                                    id: "3.1.1.1.1",
+                                    height: "10px",
+                                    name: "Как зарегистрироваться в Viomitra.Коммерческие торги",
+                                    value: "Изучите этот раздел FAQ - https://realty.viomitra.ru/registration \nВам помог наш бот?"
+                                }
+                            }
+                        },
+                        authorization: {
+                            id: "3.1.1.2",
+                            height: "10px",
+                            name: "Авторизация на Viomitra.Китай",
+                            answers: {
+                                howAuthorize: {
+                                    id: "1.1.1.2.1",
+                                    height: "10px",
+                                    name: "Как авторизоваться в Viomitra.Коммерческие торги",
+                                    value: "Чтобы войти в аккаунт нажмите 'Войти', после чего введите логин и пароль. \nВам помог наш бот?"
+                                },
+                                recoveryAccess: {
+                                    id: "3.1.1.2.2",
+                                    height: "10px",
+                                    name: "Как восстановить доступ в Viomitra.Коммерческие торги",
+                                    value: "Изучите этот раздел FAQ - https://realty.viomitra.ru/restore \nВам помог наш бот?"
+                                }
+                            }
+                        }
+                    }]
+                }
+            }]
+        }
+    }
+};
+
--- handle.js
+++ handle.js
@@ -1,87 +1,54 @@
 const { Telegraf } = require('telegraf');
 const Markup = require('telegraf/markup');
+const { faq } = require('./faq');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('6366545078:AAFZjWTJXL4RQ3rG6yvesEj-X0CciRb1JoU');
 
-let messageInfo;
+const replyStartMarkup = Markup.keyboard(
+    Object.keys(faq.sites).map(site => Markup.button.callback(faq.sites[site].name, site))
+);
 
-const replyStartMarkup = Markup.keyboard ([
-    Markup.button.callback('Viomitra.Китай', 'china'),
-    Markup.button.callback('Viomitra.Банкротство', 'bankrot')
-]);
+const replyCategoriesChinaMarkup = Markup.keyboard(
+    faq.sites.china.categories.map(category => 
+        Markup.button.callback(category.userIdentification.name, category.userIdentification.id)
+    )
+);
 
-const replyAskChinaMarkup = Markup.keyboard ([
-    Markup.button.callback('Регистрация Viomitra.Китай', 'registrationChina'),
-    Markup.button.callback('Восстановление доступа Viomotra.Китай', 'accessRecoveryChina')
-]);
+const replyCategoriesBankrotMarkup = Markup.keyboard(
+    Object.keys(faq.sites.bankrot.categories).map(category => Markup.button.callback(faq.sites.bankrot.categories[category].name, category))
+);
 
-const replyAskBankrotMarkup = Markup.keyboard ([
-    Markup.button.callback('Регистрация Viomitra.Банкротство', 'registrationBankrot'),
-    Markup.button.callback('Восстановление доступа Viomitra.Банкротство', 'accessRecoveryBankrot')
-]);
-
-const replyAskHelpMarkup = Markup.keyboard ([
-    Markup.button.callback('Помог', 'yes'),
-    Markup.button.callback('Не помог', 'no'),
-    Markup.button.callback('Задать другой вопрос', 'newAsk')
-]);
+const replyCategoriesRealtyMarkup = Markup.keyboard(
+    Object.keys(faq.sites.realty.categories).map(category => Markup.button.callback(faq.sites.realty.categories[category].name, category))
+);
 
 bot.start((ctx) => {
     ctx.replyWithMarkdown(`Привет, ${ctx.message.from.username}! С какой площадкой вам нужна помощь?`, replyStartMarkup);
 });
 
-bot.hears('Viomitra.Китай', (ctx) => {
-    ctx.replyWithMarkdown(`Вы, выбрали Viomitra.Китай. С чем связан Ваш вопрос?`, replyAskChinaMarkup);
+bot.hears(faq.sites.china.name, (ctx) => {
+    ctx.replyWithMarkdown(`Вы, выбрали Viomitra.Китай. С чем связан Ваш вопрос?`, replyCategoriesChinaMarkup);
     messageInfo = ctx.message.text;
 });
 
-bot.hears('Viomitra.Банкротство', (ctx) => {
-    ctx.replyWithMarkdown(`Вы, выбрали Viomitra.Банкротство. С чем связан Ваш вопрос?`, replyAskBankrotMarkup);
+bot.hears(faq.sites.bankrot.name, (ctx) => {
+    ctx.replyWithMarkdown(`Вы, выбрали Viomitra.Банкротство. С чем связан Ваш вопрос?`, replyCategoriesBankrotMarkup);
 });
 
-bot.hears('Регистрация Viomitra.Китай', (ctx) => {
-    ctx.replyWithMarkdown('Изучите этот раздел FAQ - https://china.viomitra.ru/registration \nВам помог наш бот?', replyAskHelpMarkup);
-    messageInfo = ctx.message.text;
+bot.hears(faq.sites.realty.name, (ctx) => {
+    ctx.replyWithMarkdown(`Вы, выбрали Viomitra.Коммерческие торги. С чем связан Ваш вопрос?`, replyCategoriesRealtyMarkup);
 });
 
-bot.hears('Регистрация Viomitra.Банкротство', (ctx) => {
-    ctx.replyWithMarkdown('Скачайте этот документ с инструкцией - https://bankrot.viomitra.ru/Document/GuideDocument?guideName=RegistrationGuideDoc \nВам помог наш бот?', replyAskHelpMarkup);
-    messageInfo = ctx.message.text;
-});
+bot.launch().then(() => console.log('Started'));
 
-bot.hears('Восстановление доступа Viomotra.Китай', (ctx) => {
-    ctx.replyWithMarkdown('Изучите этот раздел FAQ - https://china.viomitra.ru/restore \nВам помог наш бот?', replyAskHelpMarkup);
-    messageInfo = ctx.message.text;
-});
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-bot.hears('Восстановление доступа Viomitra.Банкротство', (ctx) => {
-    ctx.replyWithMarkdown('Если вы забыли пароль, вы можете воспользоваться формой "Забыли пароль?" http://bankrot.viomitra.ru/Account/ForgotPassword \nВам помог наш бот?', replyAskHelpMarkup);
-    messageInfo = ctx.message.text;
-});
-
-bot.hears('Помог', (ctx) => {
-    ctx.reply('Рады помочь!');
-});
-
-bot.hears('Не помог', (ctx) => {
-    let user = ctx.message.from;
-    let message = `Пользователю @${user.username} (${user.id}) требуется помощь. Проблема, которую выбрал пользователь: ${messageInfo}`;
-    let chatId = 797596124;
-
-    bot.telegram.sendMessage(chatId, message);
-
-    ctx.reply('С вами свяжется оператор')
-});
-
-bot.hears('Задать другой вопрос', (ctx) => {
-    ctx.replyWithMarkdown(`Привет, ${ctx.message.from.username}! С какой площадкой вам нужна помощь?`, replyStartMarkup);
-});
-
-module.exports.handler = async function (event, context) {
+/*module.exports.handler = async function (event, context) {
     const message = JSON.parse(event.body);
     await bot.handleUpdate(message);
     return {
         statusCode: 200,
         body: '',
     };
-};
+};*/
